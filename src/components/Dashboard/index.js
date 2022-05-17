d4334ed51cc27bfb2d16f9ad8af42d96d85f7479
Dameon Mendoza <dameonmendoza@gmail.com>
2022-05-17 14:03:37 -0700

    working on dashboard

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -2,54 +2,41 @@ import React from "react";
 import './dashboard.css';
 import PassportCard from "../PassportCard";
 
+function Card() {
+    let src = 'https://avatars.githubusercontent.com/u/69483974?s=200&v=4'
+    return (
+        <div className="smallContentBox">
+            <div className="smallContentImg">
+                <img alt="sometext" className="" src={src}></img>
+            </div>
+            <p>Title</p>
+            <p>Descirption</p>
+        </div>
+    )
+}
+
 
 function ContentCards() {
     return (
-        <div className="contentHolder">
-            <p>dynamic and static content by author</p>
-            <ul>
-                <li>content boxes</li>
-                <li>image</li>
-                <li>title</li>
-                <li>author</li>
-                <li>content boxes</li>
-                <li>image</li>
-                <li>title</li>
-                <li>author</li>
-                <li>content boxes</li>
-                <li>image</li>
-                <li>title</li>
-                <li>author</li>
-                <li>content boxes</li>
-                <li>image</li>
-                <li>title</li>
-                <li>author</li>
-                <li>content boxes</li>
-                <li>image</li>
-                <li>title</li>
-                <li>author</li>
-                <li>content boxes</li>
-                <li>image</li>
-                <li>title</li>
-                <li>author</li>
-                <li>content boxes</li>
-                <li>image</li>
-                <li>title</li>
-                <li>author</li>
-                <li>content boxes</li>
-                <li>image</li>
-                <li>title</li>
-                <li>author</li>
-                <li>content boxes</li>
-                <li>image</li>
-                <li>title</li>
-                <li>author</li>
-                <li>content boxes</li>
-                <li>image</li>
-                <li>title</li>
-                <li>author</li>
-            </ul>
-        </div>
+        <div className="contentScrollContainer">
+            <h1>dynamic and static content by author</h1>
+            <div className="hs">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div >
     )
 }
 
@@ -58,21 +45,8 @@ export function Dashboard() {
     return (
         <div className="dashboard">
             <PassportCard />
-
-            <div >
-                <ContentCards />
-            </div>
-            <div className="contentHolder">
-                <p>dynamic and static content by author</p>
-                <ul>
-                    <li>content boxes</li>
-                    <li>image</li>
-                    <li>title</li>
-                    <li>author</li>
-                </ul>
-            </div>
-
-
+            <ContentCards />
+            <ContentCards />
         </div>
     );
 }
--- src/components/PassportCard/index.js
+++ src/components/PassportCard/index.js
@@ -3,19 +3,22 @@ import React from "react";
 export function PassportCard() {
   let heroImage = 'https://img.youtube.com/vi/Ynfwggyl5rY/maxresdefault.jpg'
   return (
-    <div className="passport">
-      <img className="passportImage" src={heroImage} alt="heroImage" />
-      <div className="passportCard-text">
-        <ul className="passportTextUl">
-          <li className='passportText'>ANS ID</li>
-          <li className='passportText'>{"score"}</li>
-          <li className='passportText'>ArVerify score</li>
-          <li className='passportText'>{"score"}</li>
-          <li className='passportText'>Passport Did</li>
-          <li className='passportText'>{"Passport Did"}</li>
-        </ul>
+    <div className="passportContainer">
+      <div className="passport">
+        <img className="passportImage" src={heroImage} alt="heroImage" />
+        <div className="passportCard-text">
+          <ul className="passportTextUl">
+            <li className='passportText'>ANS ID</li>
+            <li className='passportText'>{"score"}</li>
+            <li className='passportText'>ArVerify score</li>
+            <li className='passportText'>{"score"}</li>
+            <li className='passportText'>Passport score</li>
+            <li className='passportText'>{"score"}</li>
+          </ul>
+        </div>
       </div>
     </div>
+
   );
 }
 
