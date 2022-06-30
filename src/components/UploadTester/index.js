d2608e36b425968e177e9f09a8572bd5160da404
Dameon Mendoza <dameonmendoza@gmail.com>
2022-06-30 10:08:35 -0700

    current state

--- src/components/UploadTester/index.js
+++ src/components/UploadTester/index.js
@@ -1,29 +1,29 @@
-import { useState, useContext, useEffect } from 'react'
-import { MainContext } from '../../context'
-//import { APP_NAME, tagSelectOptions } from '../utils'
-//import { useRouter } from 'next/router'
-//import { css } from '@emotion/css'
-import { utils } from 'ethers'
-import BigNumber from 'bignumber.js'
-//import Select from 'react-select'
-//import './upload.css';
-import { AMW } from '../../utils/api';
-import Account from 'arweave-account';
-import Select from 'react-select'
-import axios from "axios";
-import TestUploader from "../../components/TestUploader";
+// import { useState, useContext, useEffect } from 'react'
+// import { MainContext } from '../../context'
+// //import { APP_NAME, tagSelectOptions } from '../utils'
+// //import { useRouter } from 'next/router'
+// //import { css } from '@emotion/css'
+// import { utils } from 'ethers'
+// import BigNumber from 'bignumber.js'
+// //import Select from 'react-select'
+// //import './upload.css';
+// import { AMW } from '../../utils/api';
+// import Account from 'arweave-account';
+// import Select from 'react-select'
+// import axios from "axios";
+// import TestUploader from "../../components/TestUploader";
 
 
-import { bundleAndSignData, createData } from "arbundles";
+// import { bundleAndSignData, createData } from "arbundles";
 
 
-export default function UploadTester(props) {
-    const {file} = props;
+// export default function UploadTester(props) {
+//     const {file} = props;
 
-    async function uploadFile(file) {
-      const tx = await pipeline(file, createTransactionAsync(txAttrs, arweave, wallet));
-      await arweave.transactions.sign(tx, wallet);
-    }
+//     async function uploadFile(file) {
+//       const tx = await pipeline(file, createTransactionAsync(txAttrs, arweave, wallet));
+//       await arweave.transactions.sign(tx, wallet);
+//     }
 
     
-}
+// }
